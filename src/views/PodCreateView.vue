96c0f0aa239657e0976fa7e50514307467b721b3
yann <yann.boisselier@gmail.com>
2019-05-17 23:11:23 +0200

    add PodCreateView

--- src/views/PodCreateView.vue
+++ src/views/PodCreateView.vue
@@ -3,40 +3,31 @@
     <div class="content container child-is-vertical-centered">
       <div class="has-text-centered">
         <h2>
-          Glad to see you there {{ user.name }} ! 🤗 <br />First let's create a
-          new Pod.
+          Glad to see you there {{ user.name }} 🤗
+          <br>
+          <br>First let's create a new Pod. Give it a name or let me
+          <a
+            @click.prevent="onGenerateCLick"
+          >suggest you one</a>
         </h2>
-        <br />
+        <br>
 
         <div class="field">
-          <div class="label">Give a name for your Pod {{ randomName }}</div>
           <div class="control">
-            <input
-              class="input is-info is-large"
-              type="text"
-              placeholder="Pod's name"
-              value
-            />
+            <input v-model="name" class="input is-large" type="text" placeholder="Pod's Name">
           </div>
         </div>
 
-        <br />
+        <br>
+
         <div class="buttons are-medium is-centered">
-          <button class="button is-outline" @click="$router.push('/')">
-            CANCEL
-          </button>
-          <button class="button is-info" @click="showPublishBlogModal = false">
-            CREATE
-          </button>
+          <button class="button is-outline" @click="$router.push('/')">CANCEL</button>
+          <button class="button is-info" @click="showPublishBlogModal = false">CREATE MY POD</button>
         </div>
 
         <!-- Any other Bulma elements you want -->
       </div>
-      <button
-        @click="showPublishBlogModal = false"
-        class="modal-close is-large"
-        aria-label="close"
-      ></button>
+      <button @click="showPublishBlogModal = false" class="modal-close is-large" aria-label="close"></button>
     </div>
   </div>
 </template>
@@ -45,9 +36,18 @@
 import { generate } from "../lib/fantasyName.js";
 
 export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
   created() {
     this.user = this.$auth.getUser();
-    this.randomName = generate();
+  },
+  methods: {
+    onGenerateCLick() {
+      this.name = generate();
+    }
   }
 };
 </script>
