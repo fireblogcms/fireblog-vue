ad3dac4efb67499a3c2af5c3652dfc30eb976e41
yann <yann.boisselier@gmail.com>
2019-08-23 23:47:37 +0200

    fix topbar buttons

---
+++ src/components/ApiButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <span @click="onClick" target="_blank" class="item button is-outlined">
+    <img style="height:20px !important;padding-right:10px" src="/images/graphql.svg" />API
+  </span>
+</template>
+
+<script>
+export default {
+  methods: {
+    onClick() {
+      this.$emit("click");
+    }
+  }
+};
+</script>
