5357f597edc04833e5017fe2f1a48e8424e2885a
yann <yann.boisselier@gmail.com>
2019-08-24 08:44:23 +0200

    agrandissement liste nom poétque

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -72,14 +72,16 @@ export function auth0RouterMiddleware(to, from, next) {
   if (to.matched.some(record => record.meta.public === true)) {
     next();
   }
-  // do not require
+  // skip authentication in those cases:
   else if (
     to.path === "/login" ||
     to.path === "/auth0-callback" ||
     isAuthenticated()
   ) {
     next();
-  } else {
+  }
+  // in all other cases, user must be authenticated.
+  else {
     auth0client.authorize({ target: to.path });
     next(false);
   }
