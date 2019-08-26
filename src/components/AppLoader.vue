8685437159197c88989f468e766b1a40c412827a
yann <yann.boisselier@gmail.com>
2019-08-26 08:31:20 +0200

    fix lint issues

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -9,13 +9,7 @@
 
 <script>
 // https://nguyenvanduocit.github.io/vue-loading-spinner/
-import {
-  Mikepad,
-  Pencil,
-  Circle2,
-  Circle9,
-  LetterCube
-} from "vue-loading-spinner";
+import { Circle2 } from "vue-loading-spinner";
 
 export default {
   components: {
