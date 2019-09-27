f7c1565324c22b75e32e228e19c3253f2c77820e
yann <yann.boisselier@gmail.com>
2019-09-27 14:24:21 +0200

    add loading for iframely previews

--- src/components/PostForm.vue
+++ src/components/PostForm.vue
@@ -281,6 +281,7 @@ export default {
                 // If you need, set maxwidth and other styles for 'iframely-embed' class - it's yours to customize
                 '<div class="iframely-embed">' +
                 '<div class="iframely-responsive">' +
+                "loading ..." +
                 `<iframe src="${iframeUrl}" ` +
                 'frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>' +
                 "</iframe>" +
