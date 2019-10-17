7d22eec9a41024d9a6873efa432c87e842e4737e
yann <yann.boisselier@gmail.com>
2019-10-17 11:11:12 +0200

    fix apiExamples for now

--- src/apiExamples.js
+++ src/apiExamples.js
@@ -1,16 +1,11 @@
 export default ({ slug, locale }) => [
   {
-    id: "getAllPosts",
-    label: "Get all posts",
+    id: "getAllPublishedPosts",
+    label: "Get all published posts",
     snippet: `
 {
   posts {
-    pageInfo {
-      hasNextPage
-      hasPreviousPage
-      startCursor
-      endCursor
-    } 
+    totalCount
     edges {
       cursor
       node {
