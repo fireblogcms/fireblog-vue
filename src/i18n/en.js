34e493dd78729f7864f674cdff0655e4fe92d055
yann <yann.boisselier@gmail.com>
2019-12-30 14:44:24 +0100

    flatten i18n translations files

--- src/i18n/en.js
+++ src/i18n/en.js
@@ -1,152 +1,84 @@
+// prettier-ignore
 export default {
-  dictionnary: {
-    draft: "draft",
-    published: "published",
-    deleted: "deleted",
-    close: "close"
-  },
-  views: {
-    apiUsage: {
-      title: "API usage"
-    },
-    blogList: {
-      title: "My blogs",
-      createNewBlogButton: "Create a new blog",
-      settingsButton: "Settings"
-    },
-    blogSettings: {
-      generalSettingsForm: {
-        title: "General settings",
-        fields: {
-          name: {
-            label: "Name",
-            errors: {
-              required: "Field name is required"
-            }
-          },
-          description: {
-            label: "Description"
-          }
-        },
-        saveButton: "Save"
-      },
-      technicalSettingsForm: {
-        title: "Technical settings",
-        fields: {
-          url: {
-            label: "absolute url of your blog"
-          },
-          webhooks: {
-            label: "Static site rebuild webhooks",
-            help:
-              "You can specify multiple valid URLs by comma-separating them. Those webooks will each time a build of your static blog is needed."
-          }
-        },
-        saveButton: "Save"
-      },
-      dangerZone: {
-        title: "Danger zone",
-        deleteBlog: "Delete this blog"
-      }
-    },
-    postList: {
-      title: "posts",
-      firstBlogSentence: "Write your first post in this blog !",
-      firstPostWriteButton: "Write",
-      writeNewPostButton: "Write new post",
-      publishedTab: "Published",
-      draftTab: "Draft",
-      noPublishedPostFound: "No published post found for now.",
-      noDraftPostFound: "No draft post found.",
-      backToBlogLink: "My blogs",
-      deleteButton: "Delete",
-      deleteModal: {
-        title: "Delete {postTitle}",
-        content:
-          "DANGER !\r\n This action cannot be undone. {postTitle} will be deleted.",
-        confirmButton: "Delete it.",
-        cancelButton: "Oups, no, cancel!"
-      },
-      updatedOn: "updated on {date}",
-      publishedOn: "published on {date}"
-    },
-    postForm: {
-      saveDraft: "Save draft",
-      publication: "Publication",
-      unpublish: "Unpublish",
-      publishChanges: "Publish changes",
-      publishNow: "Publish now",
-      publicationCancel: "Cancel",
-      changesNotSavedModal: {
-        title: "Some changes have not been saved",
-        content: "If you quit now, your last changes will be lost",
-        saveAndQuit: "Save and quit",
-        quitWithoutSaving: "Quit without saving"
-      },
-      fields: {
-        title: {
-          placeholder: "title"
-        },
-        content: {
-          placeholder: "content"
-        },
-        featuredImage: {
-          label: "Featured image"
-        },
-        teaser: {
-          label: "Teaser",
-          help:
-            "a short introductory for your post that stimulates readers interest. 250 characters max.",
-          errors: {
-            required: "Teaser field is required."
-          }
-        },
-        slug: {
-          label: "slug",
-          help: "used to build a seo-friendly url for your post.",
-          errors: {
-            invalidCharacters:
-              "Slug can only contains minuscules and '-' characters.",
-            required: "Slug field is required"
-          }
-        }
-      },
-      previews: {
-        general: {
-          name: "Post card preview",
-          description:
-            "Approximation of how your post will appear when sharing its link on other websites like Twitter, Facebook etc:"
-        }
-      },
-      firstPublicationHurralModal: {
-        title: "Hurrah! Your post have been published!",
-        okayButton: "Okay!"
-      },
-      publishChangesHurralModal: {
-        title: "Your changes have been published!",
-        okayButton: "Okay !"
-      },
-      mediaUploadingModal: {
-        title: "We haven' finished uploading your media",
-        content:
-          "one media is currently uploading | {count} media are currently uploading",
-        cancelText: "Wait for uploads to complete!",
-        confirmText: "Quit anyway"
-      }
-    }
-  },
-  topbar: {
-    accountMenu: {
-      myAccount: "My account",
-      logout: "Logout"
-    }
-  },
-  apiModal: {
-    title: "GRAPHQL API",
-    tryItButton: "Try it!",
-    openGraphQLExplorer: "Open GraphQL explorer",
-    getAllPublishedPosts: "Get all published posts",
-    getASinglePostBySlug: "Get a single post by slug",
-    GetBlogInformations: "Get blog informations"
-  }
+  "global.draft": "Draft",
+  "global.published": "Published",
+  "global.deleted": "Deleted",
+  "global.close": "Close",
+  "views.apiUsage.title:": "Api Usage",
+  "views.blogList.title": "My blogs",
+  "views.blogList.createNewBlogButton": "Create a new blog",
+  "views.blogList.settingsButton": "Settings",
+  "views.blogSettings.generalSettingsForm.title": "General settings",
+  "views.blogSettings.generalSettingsForm.fields.name.label": "Name",
+  "views.blogSettings.generalSettingsForm.fields.name.errors.required": "Field name is required",
+  "views.blogSettings.generalSettingsForm.fields.description.label": "Description",
+  "views.blogSettings.generalSettingsForm.fields.description.help": "This description will appear in search engine results or when sharing blog's link on social networks.",
+  "views.blogSettings.generalSettingsForm.fields.image.label": "Image",
+  "views.blogSettings.generalSettingsForm.fields.image.help": "This image will appear when sharing blog's link on social networks.",
+  "views.blogSettings.generalSettingsForm.notifications.saved": "General settings saved.",
+  "views.blogSettings.generalSettingsForm.saveButton": "Save",
+  "views.blogSettings.technicalSettingsForm.title": "Technical settings",
+  "views.blogSettings.technicalSettingsForm.fields.url.label": "Absolute URL",
+  "views.blogSettings.technicalSettingsForm.fields.webhooks.label": "Webhooks to call on content changes.",
+  "views.blogSettings.technicalSettingsForm.fields.webhooks.help": "Those webooks will be triggered on content changes. You can specify multiple valid URLs by comma-separating them.",
+  "views.blogSettings.technicalSettingsForm.notifications.saved": "Technical settings saved.",
+  "views.blogSettings.technicalSettingsForm.saveButton": "Save",
+  "views.blogSettings.dangerZone.title": "Danger zone",
+  "views.blogSettings.dangerZone.deleteButton": "Delete this blog",
+  "views.blogSettings.dangerZone.deleteModal.title":  "Delete {blogName}",
+  "views.blogSettings.dangerZone.deleteModal.content":  "Please enter blog's name to confirm deletion.",
+  "views.blogSettings.dangerZone.deleteModal.cancelButton":  "Cancel",
+  "views.blogSettings.dangerZone.deleteModal.deleteButton":  "Delete",
+  "views.postList.title": "posts",
+  "views.postList.firstBlogSentence": "Time to write your first article!",
+  "views.postList.firstPostWriteButton": "Write",
+  "views.postList.writeNewPostButton": "Write a new post",
+  "views.postList.publishedTab": "Published",
+  "views.postList.draftTab": "Draft",
+  "views.postList.noPublishedPostFound": "No published post found",
+  "views.postList.noDraftPostFound": "No draft post found.",
+  "views.postList.backToBlogLink": "My blogs",
+  "views.postList.deleteButton": "Delete",
+  "views.postList.deleteModal.title": "Delete {postTitle}",
+  "views.postList.deleteModal.content": "WARNING !\r\n \"{postTitle}\" will be deleted permanently.",
+  "views.postList.deleteModal.confirmButton": "Delete",
+  "views.postList.deleteModal.cancelButton": "Oups, no, cancel !",
+  "views.postList.updatedOn": "updated on {date}",
+  "views.postList.publishedOn": "published on {date}",
+  "views.postForm.saveDraft": "Save draft",
+  "views.postForm.publication": "Publication",
+  "views.postForm.unpublish": "Unpublish",
+  "views.postForm.publishChanges": "Publish changes",
+  "views.postForm.publishNow": "Publish now",
+  "views.postForm.publicationCancel": "Cancel",
+  "views.postForm.changesNotSavedModal.title": "Some changes have not been saved",
+  "views.postForm.changesNotSavedModal.content": "If you quit now, last changes will be lost",
+  "views.postForm.changesNotSavedModal.saveAndQuit": "Save and quit",
+  "views.postForm.changesNotSavedModal.quitWithoutSaving": "Quit without saving",
+  "views.postForm.fields.title.placeholder": "Title",
+  "views.postForm.fields.content.placeholder": "Content",
+  "views.postForm.fields.content.featuredImage.label": "Image",
+  "views.postForm.fields.teaser.label": "Teaser",
+  "views.postForm.fields.teaser.help": "A short introductory for your post that stimulates readers interest. 250 characters max.",
+  "views.postForm.fields.teaser.errors.required": "Teaser field is mandatory",
+  "views.postForm.fields.slug.label": "Slug",
+  "views.postForm.fields.slug.help": "Used to build a seo-friendly url for your post.",
+  "views.postForm.fields.slug.errors.invalidCharacters": "Slug can only contains minuscules and '-' characters.",
+  "views.postForm.fields.slug.errors.required":  "Slug field is mandatory",
+  "views.postForm.firstPublicationHurralModal.title":  "Hurrah! Your post has been published!",
+  "views.postForm.firstPublicationHurralModal.okayButton":  "Okay!",
+  "views.postForm.publishChangesHurralModal.title":  "Your changes have been published!",
+  "views.postForm.publishChangesHurralModal.okayButton":  "Okay!",
+  "views.postForm.mediaUploadingModal.title": "We haven' finished uploading your media",
+  "views.postForm.mediaUploadingModal.content": "one media is currently uploading | {count} media are currently uploading",
+  "views.postForm.mediaUploadingModal.cancelText": "Wait for uploads to complete!",
+  "views.postForm.mediaUploadingModal.confirmText": "Quit anyway",
+  "topbar.accountMenu.myAccount": "My account",
+  "topbar.accountMenu.logout": "Logout",
+  "apiModal.title": "API GraphQL",
+  "apiModal.tryItButton": "Try it!",
+  "apiModal.openGraphQLExplorer": "open GraphQL Explorer",
+  "apiModal.getAllPublishedPosts": "Get all published posts",
+  "apiModal.getASinglePostBySlug":  "Get a single post by slug",
+  "apiModal.GetBlogInformations":  "Get blog informations",
 };
