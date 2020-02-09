4d6be0d47aad681e689b1956531c80770c551d30
yann <yann.boisselier@gmail.com>
2020-02-09 16:21:56 +0100

    fix css issue - was tout pété

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,3 @@
-const PurgecssPlugin = require("purgecss-webpack-plugin");
-const glob = require("glob-all");
-const path = require("path");
 const fs = require("fs");
 const dotenv = require("dotenv");
 const difference = require("lodash.difference");
@@ -83,17 +80,5 @@ module.exports = {
         prependData: `@import "@/scss/_variables.scss";`
       }
     }
-  },
-  configureWebpack: {
-    // Merged into the final Webpack config
-    plugins: [
-      new PurgecssPlugin({
-        paths: glob.sync([
-          path.join(__dirname, "./src/index.html"),
-          path.join(__dirname, "./**/*.vue"),
-          path.join(__dirname, "./src/**/*.js")
-        ])
-      })
-    ]
   }
 };
