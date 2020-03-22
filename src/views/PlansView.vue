c233fcbdca83ea5fe897b6cb0e58ab9d57a65747
yann <yann.boisselier@gmail.com>
2020-03-22 11:21:02 +0100

    add plan description for plans

--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -7,8 +7,12 @@
             <div class="column" v-for="price in prices" :key="price.planId">
               <div class="box">
                 <h2 class="title is-4">{{ price.productName }}</h2>
+                <div class="title is-6">
+                  <em>{{ price.productMetadata.SUBTITLE }}</em>
+                </div>
+                <br />
                 <div class="title is-4">
-                  {{ price.planAmount / 100 }}$ / month
+                  {{ (parseInt(price.planAmount) / 100).toFixed(2) }}$ / month
                 </div>
                 <p>
                   {{ price.productMetadata.API_CALLS_MONTH }} API calls / month
@@ -27,7 +31,7 @@
           <template v-if="prices.length === 0">
             <div class="column" v-for="(v, i) in [0, 1, 2, 3]" :key="i">
               <div class="box">
-                <ContentLoader :height="250">
+                <ContentLoader :height="350">
                   <rect x="0" y="0" rx="3" ry="3" width="100%" height="60" />
                   <rect x="0" y="80" rx="3" ry="3" width="100%" height="30" />
                   <rect x="0" y="140" rx="3" ry="3" width="100%" height="30" />
