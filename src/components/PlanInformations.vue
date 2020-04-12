4c04ee23ca8f79895ee084d7407cb9000a23cbcb
Yann <335495+yann-yinn@users.noreply.github.com>
2020-04-12 14:34:40 +0200

    use plan metadata instead of product metadata (#46)

--- src/components/PlanInformations.vue
+++ src/components/PlanInformations.vue
@@ -1,23 +1,20 @@
 <template>
   <div>
-    <p
-      class="plan-name has-text-weight-bold"
-      v-if="plan"
-    >
-      {{ $t("components.planInformations.name") }} 
-      {{ plan.productName }} 
+    <p class="plan-name has-text-weight-bold" v-if="plan">
+      {{ $t("components.planInformations.name") }}
+      {{ plan.productName }}
       <template v-if="blog.subscription.trialEnd">
-        ({{ $t("components.planInformations.freeTrial") }} 
-        {{ numberDaysLeftTrial }} 
+        ({{ $t("components.planInformations.freeTrial") }}
+        {{ numberDaysLeftTrial }}
         {{ $t("components.planInformations.daysLeftTrial") }})
       </template>
     </p>
     <ResourcesUse
       v-if="plan"
       :blogId="blog._id"
-      :callsPerMonth="plan.productMetadata.API_CALLS_MONTH"
-      :sizePerMonth="plan.productMetadata.STORAGE_GB"
-    ></ResourcesUse>
+      :callsPerMonth="plan.metadata.API_CALLS_MONTH"
+      :sizePerMonth="plan.metadata.STORAGE_GB"
+    />
   </div>
 </template>
 
@@ -39,14 +36,15 @@ export default {
     return {
       plan: null,
       numberDaysLeftTrial: null
-    }
+    };
   },
   mounted() {
     this.fetchData();
 
     if (this.blog.subscription.trialEnd) {
       this.numberDaysLeftTrial = Math.round(
-        (this.blog.subscription.trialEnd - Math.floor(new Date() / 1000)) / (3600 * 24)
+        (this.blog.subscription.trialEnd - Math.floor(new Date() / 1000)) /
+          (3600 * 24)
       );
     }
   },
