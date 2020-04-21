5ecbb52d029b0cd6392fe88fc0fd1626544856b4
LeGaby <leo.gaboriau@gmail.com>
2020-04-21 14:47:27 +0200

    Removed useless CSS class

--- src/views/PlansView.vue
+++ src/views/PlansView.vue
@@ -67,31 +67,26 @@
                   {{ (parseInt(plan.amountTaxes) / 100).toFixed(2) }}
                   {{ $t("views.plans.eurosPerMonth") }}
                 </p>
-                <p class="has-text-weight-bold">
-                  {{ $t("views.plans.includes") }}
-                </p>
-                <p>
-                  {{ plan.metadata.API_CALLS_MONTH }}
-                  {{ $t("views.plans.apiCalls") }}
-                </p>
-                <p>
-                  {{ plan.metadata.STORAGE_GB }} 
-                  {{ $t("views.plans.storageUnitGB") }} 
-                  {{ $t("views.plans.storage") }}
-                </p>
+                <div class="plan-data">
+                  <p class="has-text-weight-bold">
+                    {{ $t("views.plans.includes") }}
+                  </p>
+                  <p>
+                    {{ plan.metadata.API_CALLS_MONTH }}
+                    {{ $t("views.plans.apiCalls") }}
+                  </p>
+                  <p>
+                    {{ plan.metadata.STORAGE_GB }} 
+                    {{ $t("views.plans.storageUnitGB") }} 
+                    {{ $t("views.plans.storage") }}
+                  </p>
+                </div>
                 <button
                   @click="onSubscribeClick(plan)"
                   class="button is-primary button-subscribe"
-                  v-if="isChangePlanAvailable && !isPlanSubscribed(plan.id)"
+                  v-if="!isPlanSubscribed(plan.id)"
                 >
                   {{ $t("global.subscribeButton") }}
-                </button>
-                <button
-                  @click="onContactUsClick()"
-                  class="button is-primary button-subscribe"
-                  v-if="!isChangePlanAvailable && !isPlanSubscribed(plan.id)"
-                >
-                  {{ $t("global.contactUsButton") }}
                 </button>
               </div>
             </div>
@@ -154,9 +149,7 @@ export default {
       changePlanModal: {
         show: false,
         plan: {}
-      },
-      isChangePlanAvailable:
-        process.env.VUE_APP_CHANGE_PLAN_AVAILABLE === "true"
+      }
     };
   },
   created() {
@@ -230,9 +223,6 @@ export default {
           throw new Error(error);
         });
     },
-    onContactUsClick() {
-      $crisp.push(["do", "chat:open"]);
-    },
     async fetchData() {
       this.blog = await getBlog(this.$route.params.blogId);
       if (this.blog.subscription.trialEnd) {
@@ -279,7 +269,7 @@ export default {
 .box-subscribed-plan {
   border-color: $primary;
 }
-.button-subscribe {
-  margin-top: 2rem;
+.plan-data {
+  margin-bottom: 2rem;
 }
 </style>
