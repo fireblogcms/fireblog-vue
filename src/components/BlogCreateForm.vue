2e4a48502086ad4a19a7a97cdef91c4e0b2114d8
LeGaby <leo.gaboriau@gmail.com>
2020-04-30 15:29:49 +0200

    Added disabled button

--- src/components/BlogCreateForm.vue
+++ src/components/BlogCreateForm.vue
@@ -1,65 +1,52 @@
 <template>
-  <div>
-    <template v-if="initDataState === 'PENDING'">
-      <AppLoader>Loading</AppLoader>
-    </template>
+  <div class="text-center">
+    <!-- special text if this is the very first blog :) -->
+    <div v-if="isMyFirstBlog && user" class="mb-12">
+      <p class="text-3xl font-bold">{{ $t("views.blogCreate.gladToSeeYouHere") }}, {{ user.name }} 🤗</p>
+      <p class="text-2xl">{{ $t("views.blogCreate.createFirstBlog") }}</p>
+    </div>
 
-    <template v-if="initDataState === 'FINISHED_ERROR'">
-      <div class="text-red-600">{{ initStateError }}</div>
-    </template>
+    <div>
+      <label class="text-2xl font-bold">{{ $t("views.blogCreate.fields.title.label") }}</label>
+      <p class="text-sm mb-4">{{ $t("views.blogCreate.fields.title.help") }}</p>
+      <AppInput
+        v-model="inputs.name"
+        :error="formErrors.name"
+        type="text"
+        maxlength="250"
+        :placeholder="generate()"
+      />
+    </div>
 
-    <template v-if="initDataState === 'FINISHED_OK'">
-      <div class="text-center">
-        <!-- special text if this is the very first blog :) -->
-        <div v-if="isMyFirstBlog" class="mb-12">
-          <p class="text-3xl font-bold">{{ $t("views.blogCreate.gladToSeeYouHere") }}, {{ user.name }} 🤗</p>
-          <p class="text-2xl">{{ $t("views.blogCreate.createFirstBlog") }}</p>
-        </div>
+    <div class="mt-10 mb-16">
+      <label class="text-2xl font-bold">{{ $t("views.blogCreate.fields.description.label") }}</label>
+      <p class="text-sm mb-4">{{ $t("views.blogCreate.fields.description.help") }}</p>
+      <AppTextarea
+        v-model="inputs.description"
+        type="text"
+        maxlength="250"
+      />
+    </div>
 
-        <div>
-          <label class="text-2xl font-bold">{{ $t("views.blogCreate.fields.title.label") }}</label>
-          <p class="text-sm mb-4">{{ $t("views.blogCreate.fields.title.help") }}</p>
-          <AppInput
-            v-model="inputs.name"
-            :error="formErrors.name"
-            type="text"
-            maxlength="250"
-            :placeholder="generate()"
-          />
-        </div>
-
-        <div class="mt-10 mb-16">
-          <label class="text-2xl font-bold">{{ $t("views.blogCreate.fields.description.label") }}</label>
-          <p class="text-sm mb-4">{{ $t("views.blogCreate.fields.description.help") }}</p>
-          <AppTextarea
-            v-model="inputs.description"
-            type="text"
-            maxlength="250"
-          />
-        </div>
-
-        <div class="flex justify-center">
-          <AppButton
-            v-if="!isMyFirstBlog"
-            type="primary-outlined"
-            class="mx-4"
-            @click="$router.push('/')"
-          >
-            {{ $t("views.blogCreate.cancelButton").toUpperCase() }}
-          </AppButton>
-          <!-- TODO: Add button with loader and remove disabled attribute -->
-          <AppButton
-            :disabled="savingBlogState === 'PENDING'"
-            class="mx-4"
-            :class="{ 'is-loading': savingBlogState === 'PENDING' }"
-            type="primary"
-            @click="onCreateClick"
-          >
-            {{ $t("views.blogCreate.createButton").toUpperCase() }}
-          </AppButton>
-        </div>
-      </div>
-    </template>
+    <div class="flex justify-center">
+      <AppButton
+        v-if="!isMyFirstBlog"
+        type="primary-outlined"
+        class="mx-4"
+        @click="$router.push('/')"
+      >
+        {{ $t("views.blogCreate.cancelButton").toUpperCase() }}
+      </AppButton>
+      <!-- TODO: Add button with loader and remove disabled attribute -->
+      <AppButton
+        :disabled="savingBlogState === 'PENDING'"
+        class="mx-4"
+        type="primary"
+        @click="onCreateClick"
+      >
+        {{ $t("views.blogCreate.createButton").toUpperCase() }}
+      </AppButton>
+    </div>
   </div>
 </template>
 
@@ -76,15 +63,12 @@ import {
   getUserQuery,
   createBlogMutation
 } from "@/utils/queries";
-import AppLoader from "@/components/AppLoader";
-import logger from "@/utils/logger";
 
 export default {
   components: {
     AppButton,
     AppInput,
-    AppTextarea,
-    AppLoader
+    AppTextarea
   },
   props: {
     isMyFirstBlog: {
@@ -95,8 +79,6 @@ export default {
     return {
       errors: [],
       formErrors: {},
-      initDataState: REQUEST_STATE.NOT_STARTED,
-      initStateError: null,
       savingBlogState: REQUEST_STATE.NOT_STARTED,
       user: null,
       languageList: null,
@@ -112,16 +94,9 @@ export default {
   },
   methods: {
     async initData() {
-      this.initStateError = null;
-      this.initDataState = REQUEST_STATE.PENDING;
-      Promise.all([getUser()])
-        .then(([user]) => {
-          this.initDataState = REQUEST_STATE.FINISHED_OK;
-          this.user = user;
-        })
+      getUser()
+        .then(user => this.user = user)
         .catch(error => {
-          this.initDataState = REQUEST_STATE.FINISHED_ERROR;
-          this.initStateError = "initData() : " + error;
           throw new Error(error);
         });
     },
@@ -149,11 +124,11 @@ export default {
           }
         })
         .then(async result => {
-          this.savingBlogState = REQUEST_STATE.FINISHED_OK;
           this.$router.push({
             name: "postList",
             params: { blogId: result.data.createBlog._id }
           });
+          this.savingBlogState = REQUEST_STATE.FINISHED_OK;
         })
         .catch(error => {
           this.savingBlogState = REQUEST_STATE.FINISHED_ERROR;
--- src/ui-kit/AppButton.vue
+++ src/ui-kit/AppButton.vue
@@ -5,9 +5,13 @@
       'py-3 px-6 text-2xl': size === '',
       'py-1 px-4 text-xl': size === 'small',
       'bg-white border-gray-200': type === '',
-      'bg-primary border-primary text-white hover:bg-primary-darker hover:border-primary-darker active:bg-primary-dark active:border-primary-dark': type === 'primary',
-      'bg-white border-primary text-primary hover:border-primary-darker hover:text-primary-darker active:border-primary-dark active:text-primary-dark': type === 'primary-outlined'
+      'bg-primary border-primary text-white active:bg-primary-dark active:border-primary-dark': type === 'primary',
+      'bg-white border-primary text-primary active:border-primary-dark active:text-primary-dark': type === 'primary-outlined',
+      'hover:bg-primary-darker hover:border-primary-darker': type === 'primary' && !$attrs.disabled,
+      'hover:border-primary-darker hover:text-primary-darker': type === 'primary-outlined' && !$attrs.disabled,
+      'cursor-default opacity-50': $attrs.disabled
     }"
+    v-bind="$attrs"
     @click="onClick"
   >
     <slot></slot>
