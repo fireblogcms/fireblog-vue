53f3f6d0bbf44d64a5332d5ddb6146c786eba2f9
LeGaby <leo.gaboriau@gmail.com>
2020-05-01 09:30:52 +0200

    Changed loader

--- src/components/AppLoader.vue
+++ src/components/AppLoader.vue
@@ -1,39 +1,5 @@
 <template>
-  <div
-    :style="wrapperStyle()"
-    class="app-loader-wrapper has-text-centered section"
-  >
-    <Circle2 background="#9584e1" style="display:inline-block" />
-    <div>
-      <slot />
-    </div>
+  <div class="my-24 flex justify-center">
+    <img class="w-40 h-40" src="/images/loader.svg" />
   </div>
 </template>
-
-<script>
-// https://nguyenvanduocit.github.io/vue-loading-spinner/
-import { Circle2 } from "vue-loading-spinner";
-
-export default {
-  components: {
-    Circle2
-  },
-  props: {
-    absolute: {
-      type: Boolean,
-      default: false
-    }
-  },
-  methods: {
-    wrapperStyle() {
-      const styles = {};
-      if (this.absolute === true) {
-        styles.position = "absolute";
-        styles.margin = "auto";
-        styles.width = "100vw";
-      }
-      return styles;
-    }
-  }
-};
-</script>
