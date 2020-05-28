3efdbb3d23b5a4f1f5c26912eb8c522ee0267efb
yann <yann.boisselier@gmail.com>
2020-05-28 19:29:47 +0200

    remettre la largeur des images comme sur la prod

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -16,38 +16,34 @@
         class="px-8 py-4 md:py-6 flex flex-col md:flex-row cursor-pointer border-b border-gray-300 last:border-b-0"
         v-for="post in posts.edges"
         :key="post.node._id"
-        @click="$router.push({
-          name: 'postUpdate',
-          params: {
-            blogId: $route.params.blogId,
-            postId: post.node._id
-          }
-        })"
+        @click="
+          $router.push({
+            name: 'postUpdate',
+            params: {
+              blogId: $route.params.blogId,
+              postId: post.node._id
+            }
+          })
+        "
       >
         <div class="flex flex-1">
           <div
             v-show="post.node.image"
             v-lazy:background-image="post.node.image"
-            class="w-32 mr-10 rounded-md bg-center bg-no-repeat bg-contain"
+            class="w-64 mr-10 rounded-md bg-center bg-no-repeat bg-contain"
           ></div>
           <div>
             <p class="text-3xl font-bold">
               {{ post.node.title }}
             </p>
-            <p
-              class="text-sm italic"
-              v-if="post.node.status === 'PUBLISHED'"
-            >
+            <p class="text-sm italic" v-if="post.node.status === 'PUBLISHED'">
               {{
                 $t("views.postList.publishedOn", {
                   date: publishedOnDate(post)
                 })
               }}
             </p>
-            <p
-              class="text-sm italic"
-              v-if="post.node.status === 'DRAFT'"
-            >
+            <p class="text-sm italic" v-if="post.node.status === 'DRAFT'">
               {{
                 $t("views.postList.updatedOn", {
                   date: updatedOnDate(post)
