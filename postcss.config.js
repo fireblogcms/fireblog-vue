41cbb0de80312d971ca802388edcfa0f6d2873bb
yann <yann.boisselier@gmail.com>
2020-11-28 20:25:29 +0100

    try without purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,38 +1,3 @@
 module.exports = {
-  plugins: [
-    require("tailwindcss")(),
-    ...(process.env.NODE_ENV !== "development"
-      ? [
-          require("@fullhuman/postcss-purgecss")({
-            content: ["./public/**/*.html", "./src/**/*.vue"],
-            defaultExtractor: content => {
-              const contentWithoutStyleBlocks = content.replace(
-                /<style[^]+?<\/style>/gi,
-                ""
-              );
-              return (
-                contentWithoutStyleBlocks.match(
-                  /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-                ) || []
-              );
-            },
-            whitelistPatterns: [
-              /-(leave|enter|appear)(|-(to|from|active))$/,
-              /^(?!(|.*?:)cursor-move).+-move$/,
-              /^router-link(|-exact)-active$/,
-              /^app-toast-/,
-            ],
-            whitelistPatternsChildren: [
-              // for prismJS component
-              /^language-/,
-              /^token/,
-              /^pre/,
-              /^code/,
-              /^tag-auto-complete/,
-            ],
-          }),
-        ]
-      : []),
-    require("autoprefixer")(),
-  ],
+  plugins: [require("tailwindcss")(), require("autoprefixer")()],
 };
