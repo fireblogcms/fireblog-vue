07ed28b3b1949055499652d62e560fcfd590bccc
yann <yann.boisselier@gmail.com>
2020-11-28 20:30:38 +0100

    put back purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,27 @@
 module.exports = {
-  plugins: [require("tailwindcss")(), require("autoprefixer")()],
+  plugins: [
+    require("tailwindcss")(),
+    ...(process.env.NODE_ENV !== "development"
+      ? [
+          require("@fullhuman/postcss-purgecss")({
+            content: ["./public/**/*.html", "./src/**/*.vue"],
+            whitelistPatterns: [
+              /-(leave|enter|appear)(|-(to|from|active))$/,
+              /^(?!(|.*?:)cursor-move).+-move$/,
+              /^router-link(|-exact)-active$/,
+              /^app-toast-/,
+            ],
+            whitelistPatternsChildren: [
+              // for prismJS component
+              /^language-/,
+              /^token/,
+              /^pre/,
+              /^code/,
+              /^tag-auto-complete/,
+            ],
+          }),
+        ]
+      : []),
+    require("autoprefixer")(),
+  ],
 };
