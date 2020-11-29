bf711ccb420e34f612318033c1ddb62cb8a72ec3
yann <yann.boisselier@gmail.com>
2020-11-29 20:05:10 +0100

    remove focus:bg-white for now, does not work with @apply

--- src/ui-kit/AppFieldText.vue
+++ src/ui-kit/AppFieldText.vue
@@ -7,7 +7,7 @@
       {{ help }}
     </div>
     <input
-      class="p-4 text-xl bg-gray-100 border border-gray-200 shadow-sm appearance-none rounded-lg w-full text-current focus:outline-none focus:shadow-outline focus:bg-white"
+      class="p-4 text-xl bg-gray-100 border border-gray-200 shadow-sm appearance-none rounded-lg w-full text-current focus:outline-none focus:shadow-outline"
       type="text"
       :value="value"
       @input="$emit('input', $event.target.value)"
