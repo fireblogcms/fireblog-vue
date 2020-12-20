f8de3d6dad6abdd3517777deb9fb0838a41fb0dc
yann <yann.boisselier@gmail.com>
2020-12-20 15:16:46 +0100

    update background image

--- src/layouts/DefaultLayout.vue
+++ src/layouts/DefaultLayout.vue
@@ -9,10 +9,17 @@
 
 <script>
 import AppTopbar from "@/ui-kit/AppTopbar";
+import { getBlog } from "@/utils/helpers";
 
 export default {
   components: {
     AppTopbar,
+  },
+  data() {
+    return {
+      style: {},
+      blog: null,
+    };
   },
   methods: {
     isTopbarVisible() {
@@ -20,12 +27,23 @@ export default {
       return !hideForRoutes.includes(this.$route.name);
     },
   },
-  computed: {
-    style() {
-      const backgroundImage = this.$store.state.global.backgroundImage;
-      return {
-        background: `center / cover url("${backgroundImage}")`,
-      };
+  watch: {
+    $route: {
+      handler: function(value) {
+        if (this.$route.params.blogId) {
+          getBlog(this.$route.params.blogId).then(blog => {
+            this.blog = blog.backgroundImage;
+            if (this.backgroundImage != blog.backgroundImage) {
+              this.style = {
+                background: `center / cover url("${blog.backgroundImage}")`,
+              };
+            }
+          });
+        } else {
+          this.style = {};
+        }
+      },
+      immediate: true,
     },
   },
 };
